661120e62a4d5ead937454b1e9a2a7541daccaff
Vladislav Nevraev <daredevil2309@gmail.com>
2026-03-18 22:48:37 +0300

    move by spline

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace DefaultNamespace
+{
+    public class Character : Pawn
+    {
+        [SerializeField] private Path _path;
+        [SerializeField] private float _movementSpeed;
+
+        private float _distance;
+        
+        public void Update()
+        {
+            _distance += _movementSpeed * Time.deltaTime;
+            var t = SplineUtility.GetNormalizedInterpolation(_path.Value, _distance, PathIndexUnit.Distance);
+            transform.position = _path.Value.EvaluatePosition(t);
+            transform.forward = math.normalize(_path.Value.EvaluateTangent(t));
+        }
+    }
+}
